d67fddb5ce97e19b531b107b36b7c12c183e504b
Ajith01617 <ajithasha0085@gmail.com>
2025-09-22 13:36:36 +0530

    Car_PojoDEMO

---
+++ Day1/pojodemo.java
@@ -0,0 +1,40 @@
+ class Car{
+    private String name;
+    private String colour;
+    private Integer price;
+
+    public String getName() {
+        return name;
+    }
+    public String getColour() {
+        return colour;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+        return "Car [name=" + name + ", colour=" + colour + ", price=" + price + "]";
+    }
+
+}
+
+public class pojodemo{
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setName("BMW");
+        car.setColour("black"); 
+        car.setPrice(100000);
+        System.out.println(car);
+    }
+}
